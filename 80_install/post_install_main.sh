a9e4f6efb8eda9f1d411dcc12e5127fae8f5897e
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-06-08 15:22:50 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -5,8 +5,11 @@ set -x
 date
 
 pushd www
-git clone --depth 1 https://git.tt-rss.org/fox/tt-rss.git
-mv tt-rss ttrss
+
+git clone --depth 1 https://git.tt-rss.org/fox/tt-rss.git ttrss
+
 popd
+
+chmod 755 ./start_web.sh
 
 date
