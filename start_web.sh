f8bcaa4ee52539664a1247338fd22c7adda6c69e
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-06-08 16:25:39 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -4,6 +4,8 @@ set -x
 
 export TZ=JST-9
 
+whereis php
+
 export postgres_user=$(echo ${DATABASE_URL} | awk -F':' '{print $2}' | sed -e 's/\///g')
 export postgres_password=$(echo ${DATABASE_URL} | grep -o '/.\+@' | grep -o ':.\+' | sed -e 's/://' | sed -e 's/@//')
 export postgres_server=$(echo ${DATABASE_URL} | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
