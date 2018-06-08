9aa2054979c44c2d58322ea9275db35f93855ee6
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-06-08 15:58:55 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -4,4 +4,9 @@ set -x
 
 export TZ=JST-9
 
+export postgres_user=$(echo ${DATABASE_URL} | awk -F':' '{print $2}' | sed -e 's/\///g')
+export postgres_password=$(echo ${DATABASE_URL} | grep -o '/.\+@' | grep -o ':.\+' | sed -e 's/://' | sed -e 's/@//')
+export postgres_server=$(echo ${DATABASE_URL} | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
+export postgres_dbname=$(echo ${DATABASE_URL} | awk -F'/' '{print $NF}')
+
 vendor/bin/heroku-php-apache2 -C apache.conf www
