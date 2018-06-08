8092b1c3116ed98701c7ceae147126a85d760630
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-06-08 16:32:41 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -13,8 +13,8 @@ export postgres_dbname=$(echo ${DATABASE_URL} | awk -F'/' '{print $NF}')
 
 cp config.php www/ttrss/
 
-mkdir -m 666 -p /tmp/lock
-mkdir -m 666 -p /tmp/cache
-mkdir -m 666 -p /tmp/feed-icons
+mkdir -m 777 -p /tmp/lock
+mkdir -m 777 -p /tmp/cache/images
+mkdir -m 777 -p /tmp/feed-icons
 
 vendor/bin/heroku-php-apache2 -C apache.conf www
