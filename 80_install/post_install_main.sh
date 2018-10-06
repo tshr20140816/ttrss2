299a93c242fa47a6e96e940a105a178057ad0fb4
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-07 03:11:54 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -8,6 +8,10 @@ pushd www
 
 git clone --depth 1 https://git.tt-rss.org/fox/tt-rss.git ttrss
 
+git clone --depth 1 https://github.com/phppgadmin/phppgadmin.git phppgadmin
+cp ../config.inc.php phppgadmin/conf/
+cp ../Connection.php phppgadmin/classes/database/
+
 popd
 
 chmod 755 ./start_web.sh
