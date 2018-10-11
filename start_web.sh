9818d46bf269aa1ab16bec05030f463e99ab9f1f
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-10-11 11:52:59 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -28,6 +28,11 @@ if [ ! -v BASIC_PASSWORD ]; then
   exit
 fi
 
+export HOME_IP_ADDRESS=$(nslookup ${HOME_FQDN} 8.8.8.8 | tail -n2 | grep -o '[0-9]\+.\+')
+if [ -z "${HOME_IP_ADDRESS}" ]; then
+  HOME_IP_ADDRESS=127.0.0.1
+fi
+
 htpasswd -c -b .htpasswd ${BASIC_USER} ${BASIC_PASSWORD}
 
 vendor/bin/heroku-php-apache2 -C apache.conf www
