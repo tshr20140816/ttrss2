f079cb698f8fbb8d0f81e3881addd119e3e4fd9e
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-16 18:55:30 +0900

    Update rss01.php

--- www/rss01.php
+++ www/rss01.php
@@ -7,3 +7,41 @@ $pdo = new PDO(
     $connection_info['user'],
     $connection_info['pass']
 );
+
+$sql = <<< __HEREDOC__
+SELECT SUM(T1.reltuples) cnt
+  FROM pg_class T1
+ WHERE EXISTS ( SELECT 'X'
+                  FROM pg_stat_user_tables T2
+                 WHERE T2.relname = T1.relname
+                   AND T2.schemaname='public'
+              )
+__HEREDOC__;
+
+$pdo = $mu_->get_pdo();
+$count = 0;
+foreach ($pdo->query($sql) as $row) {
+    error_log(print_r($row, true));
+    $count = $row['cnt'];
+}
+
+$pdo = null;
+
+header('Content-Type: application/xml');
+
+$xml_text = <<< __HEREDOC__
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+  <channel>
+    <title>quota</title>
+    <link>https://www.heroku.com/</link>
+    <description>none</description>
+    <language>ja</language>
+    <item><title>__TITLE__</title><link>https://www.heroku.com/</link><description>__DESCRIPTION__</description><pubDate/></item>
+  </channel>
+</rss>
+__HEREDOC__;
+
+$xml_text = str_replace('__TITLE__', $count, $xml_text);
+$xml_text = str_replace('__DESCRIPTION__', date(' Y/m/d H:i', strtotime('+9 hours')), $xml_text);
+echo $xml_text;
