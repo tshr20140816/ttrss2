6f63f691b16ec652cb4d141329bd0f0f4b0108f1
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-16 19:01:06 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -12,7 +12,7 @@ cp ../config.php ttrss/
 
 git clone --depth 1 https://github.com/phppgadmin/phppgadmin.git phppgadmin
 cp ../config.inc.php phppgadmin/conf/
-cp ../Connection.php phppgadmin/classes/database/
+# cp ../Connection.php phppgadmin/classes/database/
 
 popd
 
