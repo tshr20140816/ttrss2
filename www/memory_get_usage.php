f55a0a6f610decb4fbd9ea2ae0be81f043df125b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-07-23 15:38:33 +0900

    Update and rename memory_get_usage.php to memory_usage.php

--- www/memory_get_usage.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-error_log('memory_get_usage : ' . number_format(memory_get_usage()) . 'byte');
---
+++ www/memory_usage.php
@@ -0,0 +1,4 @@
+<?php
+
+error_log('memory_get_usage : ' . number_format(memory_get_usage()) . 'byte');
+error_log('memory_get_peak_usage : ' . number_format(memory_get_peak_usage()) . 'byte');
