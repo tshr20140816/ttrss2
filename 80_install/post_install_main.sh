cefdb3f797878f2b9661cf94ef58a6c86808ab3c
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-18 21:38:46 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -13,9 +13,11 @@ git clone --depth 1 -b 19.8 https://git.tt-rss.org/fox/tt-rss.git ttrss
 cp ../config.php ttrss/
 
 wait
+
 cp ../config.inc.php phppgadmin/conf/
 mv ttrss/include/functions.php ttrss/include/functions.php.org
-cp ../functions.php ttrss/include/
+# cp ../functions.php ttrss/include/
+cp ../functions.19.8.php ttrss/include/functions.php
 diff ttrss/include/functions.php.org ttrss/include/functions.php
 popd
 
