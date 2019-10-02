ff8a32b0201ca98a480d2883c76ebf8283efef29
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-10-02 16:16:44 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -6,11 +6,10 @@ date
 
 pushd www
 
-git clone --depth=1 -b REL_5-6-0 https://github.com/phppgadmin/phppgadmin.git phppgadmin &
-git clone --depth 1 -b 19.8 https://git.tt-rss.org/fox/tt-rss.git ttrss
+git clone --depth=1 -b REL_5-6-0 https://github.com/phppgadmin/phppgadmin.git phppgadmin
+# git clone --depth 1 -b 19.8 https://git.tt-rss.org/fox/tt-rss.git ttrss
+git clone --depth=1 -b master https://github.com/tshr20140816/tt-rss.git ttrss
 cp ../config.php ttrss/
-
-wait
 
 cp ../config.inc.php phppgadmin/conf/
 mv ttrss/include/functions.php ttrss/include/functions.php.org
