8ea7e7ab37e5e8c80423cf83c29fe4c333ea41e4
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-10-09 14:52:36 +0900

    Delete brotli.sh

--- brotli.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x
-
-ls www/ttrss/css/*.css | xargs -n 3 brotli -Z
-ls www/ttrss/themes/*.css | xargs -n 3 brotli -Z
-
-ls www/ttrss/js/*.js | xargs -n 3 brotli -Z
-ls www/ttrss/js/form/*.js | xargs -n 3 brotli -Z
